b3e806e5b87d7a935ccda8b9d5abab6eb485834f
Angelina Ebreo <angelinaebreo@gmail.com>
2021-06-15 17:04:09 -0400

    initil commit

---
+++ server.js
@@ -0,0 +1,14 @@
+//Dependencies
+const app = require("./app.js")
+
+//Configuration
+require("dotenv").config();
+
+//Port
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+
+
+//Listen
+app.listen(PORT, () => console.log("Listening on port:", PORT));
